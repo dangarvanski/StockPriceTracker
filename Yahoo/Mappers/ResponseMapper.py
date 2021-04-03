83e99fe8b5462ad08980b30897a0733fa9dcf65a
Dan Garvanski <danail.cupper@gmail.com>
2021-04-03 12:18:19 +0100

    ExperimentingWithAutoGeneratedResponse
    As I need a universal response that can handle both crypto and company stock, it would take me too much time to waste to build it myself so I used an auto generator for the request and I'll just have to do the mapping.
    This is a mocked up version of the mapping.

--- Yahoo/Mappers/ResponseMapper.py
+++ Yahoo/Mappers/ResponseMapper.py
@@ -1,26 +1,41 @@
 from Yahoo import YahooResponse
+from Yahoo import YahooResponseV2
 from Yahoo.Mappers import PriceMapper
 from Yahoo.Mappers import QuoteTypeMapper
 from Yahoo.Mappers import SummaryDetailMapper
 from Yahoo.Mappers import AssetProfileMapper
 
 
-def map_response(json_reponse):
+def map_response(json_response):
     response = YahooResponse.YahooResponse
 
-    mappedPrice = PriceMapper.map_price(json_reponse["price"])
-    mappedQuoteType = QuoteTypeMapper.map_quote_type(json_reponse["quoteType"])
-    mappedSummaryDetail = SummaryDetailMapper.map_summary_detail(json_reponse["summaryDetail"])
-    mappedAssetProfile = AssetProfileMapper.map_asset_profile(json_reponse["assetProfile"])
+    mappedPrice = PriceMapper.map_price(json_response["price"])
+    mappedQuoteType = QuoteTypeMapper.map_quote_type(json_response["quoteType"])
+    mappedSummaryDetail = SummaryDetailMapper.map_summary_detail(json_response["summaryDetail"])
+    mappedAssetProfile = AssetProfileMapper.map_asset_profile(json_response["assetProfile"])
 
-    response.FinancialsTemplate = json_reponse["financialsTemplate"]
+    response.FinancialsTemplate = json_response["financialsTemplate"]
     response.Price = mappedPrice
-    response.SecFilings = json_reponse["secFilings"]
+    response.SecFilings = json_response["secFilings"]
     response.QuoteType = mappedQuoteType
-    response.CalendarEvents = json_reponse["calendarEvents"]
+    response.CalendarEvents = json_response["calendarEvents"]
     response.SummaryDetail = mappedSummaryDetail
-    response.Symbol = json_reponse["symbol"]
+    response.Symbol = json_response["symbol"]
     response.AssetProfile = mappedAssetProfile
-    response.PageViews = json_reponse["pageViews"]
+    response.PageViews = json_response["pageViews"]
 
+    return response
+
+
+def map_responseV2(json_response):
+    response = YahooResponseV2
+
+    financeTemplate = json_response["financialsTemplate"]
+
+    if len(financeTemplate) != 0:
+        response.FinancialsTemplate.code = financeTemplate["code"]
+        response.FinancialsTemplate.max_age = financeTemplate["maxAge"]
+    else:
+        response.FinancialsTemplate.code = "Empty bruh"
+        response.FinancialsTemplate.max_age = "It's empty"
     return response
--- main.py
+++ main.py
@@ -11,7 +11,11 @@ response = YahooSubmitter.submit(request)
 #Coverting response to json
 responseJson = json.loads(response.text)
 
-responsePython = ResponseMapper.map_response(responseJson)
+#responsePython = ResponseMapper.map_response(responseJson)
+responsePythonV2 = ResponseMapper.map_responseV2(responseJson)
 
-print('Assets Company Officers: ' + str(responsePython.AssetProfile.CompanyOfficers))
-print('Assets Name: ' + str(responsePython.AssetProfile.Name))
+#print('Assets Company Officers: ' + str(responsePython.AssetProfile.CompanyOfficers))
+#print('Assets Name: ' + str(responsePython.AssetProfile.Name))
+
+print('Response V2: ' + str(responsePythonV2.FinancialsTemplate.code))
+print('Response V2: ' + str(responsePythonV2.FinancialsTemplate.max_age))
