3898c3bcc64b3e52e97499e93eae6f34a8e08492
Dan Garvanski <danail.cupper@gmail.com>
2021-04-06 18:03:42 +0100

    RevertingChanged
    Reverting Rapid API integration related changes

--- Yahoo/YahooSubmitter.py
+++
@@ -1,8 +0,0 @@
-from Yahoo import YahooClient
-
-
-def submit(request):
-    request_obj = {"symbol": request.get_symbol(), "region": request.get_region()}
-
-    response_obj = YahooClient.get_profile(request_obj)
-    return response_obj
