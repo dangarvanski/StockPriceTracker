26e2c2a0577c80f261057efe064b5be9e221d553
Dan Garvanski <danail.cupper@gmail.com>
2021-04-06 18:07:34 +0100

    Reverting Changes
    Reverting Changes

--- Yahoo/Mappers/ResponseMapper.py
+++ Yahoo/Mappers/ResponseMapper.py
@@ -1,41 +1,26 @@
 from Yahoo import YahooResponse
-from Yahoo import YahooResponseV2
 from Yahoo.Mappers import PriceMapper
 from Yahoo.Mappers import QuoteTypeMapper
 from Yahoo.Mappers import SummaryDetailMapper
 from Yahoo.Mappers import AssetProfileMapper
 
 
-def map_response(json_response):
+def map_response(json_reponse):
     response = YahooResponse.YahooResponse
 
-    mappedPrice = PriceMapper.map_price(json_response["price"])
-    mappedQuoteType = QuoteTypeMapper.map_quote_type(json_response["quoteType"])
-    mappedSummaryDetail = SummaryDetailMapper.map_summary_detail(json_response["summaryDetail"])
-    mappedAssetProfile = AssetProfileMapper.map_asset_profile(json_response["assetProfile"])
+    mappedPrice = PriceMapper.map_price(json_reponse["price"])
+    mappedQuoteType = QuoteTypeMapper.map_quote_type(json_reponse["quoteType"])
+    mappedSummaryDetail = SummaryDetailMapper.map_summary_detail(json_reponse["summaryDetail"])
+    mappedAssetProfile = AssetProfileMapper.map_asset_profile(json_reponse["assetProfile"])
 
-    response.FinancialsTemplate = json_response["financialsTemplate"]
+    response.FinancialsTemplate = json_reponse["financialsTemplate"]
     response.Price = mappedPrice
-    response.SecFilings = json_response["secFilings"]
+    response.SecFilings = json_reponse["secFilings"]
     response.QuoteType = mappedQuoteType
-    response.CalendarEvents = json_response["calendarEvents"]
+    response.CalendarEvents = json_reponse["calendarEvents"]
     response.SummaryDetail = mappedSummaryDetail
-    response.Symbol = json_response["symbol"]
+    response.Symbol = json_reponse["symbol"]
     response.AssetProfile = mappedAssetProfile
-    response.PageViews = json_response["pageViews"]
+    response.PageViews = json_reponse["pageViews"]
 
-    return response
-
-
-def map_responseV2(json_response):
-    response = YahooResponseV2
-
-    financeTemplate = json_response["financialsTemplate"]
-
-    if len(financeTemplate) != 0:
-        response.FinancialsTemplate.code = financeTemplate["code"]
-        response.FinancialsTemplate.max_age = financeTemplate["maxAge"]
-    else:
-        response.FinancialsTemplate.code = "Empty bruh"
-        response.FinancialsTemplate.max_age = "It's empty"
     return response
